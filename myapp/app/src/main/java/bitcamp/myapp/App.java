a7ba59fa2fbb0bfef082c5007b46fdfb2ddb8348
eomjinyoung <jinyoung.eom@gmail.com>
2023-12-01 12:49:27 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -28,44 +28,7 @@ public class App {
 
       switch (input) {
         case "1":
-          System.out.println("[과제]");
-          System.out.println("1. 등록");
-          System.out.println("2. 조회");
-          System.out.println("3. 변경");
-          System.out.println("4. 삭제");
-          System.out.println("0. 이전");
-
-          loop2:
-          while (true) {
-            input = prompt("메인/과제", keyIn);
-
-            switch (input) {
-              case "1":
-                System.out.println("등록입니다.");
-                break;
-              case "2":
-                System.out.println("조회입니다.");
-                break;
-              case "3":
-                System.out.println("변경입니다.");
-                break;
-              case "4":
-                System.out.println("삭제입니다.");
-                break;
-              case "0":
-                break loop2;
-              case "menu":
-                System.out.println("[과제]");
-                System.out.println("1. 등록");
-                System.out.println("2. 조회");
-                System.out.println("3. 변경");
-                System.out.println("4. 삭제");
-                System.out.println("0. 이전");
-                break;
-              default:
-                System.out.println("메뉴 번호가 옳지 않습니다!");
-            }
-          }
+          onAssignment(keyIn);
           break;
         case "2":
           System.out.println("게시글입니다.");
@@ -107,6 +70,46 @@ public class App {
   static String prompt(String title, Scanner keyIn) {
     System.out.printf("%s> ", title);
     return keyIn.nextLine();
+  }
+
+  static void onAssignment(Scanner keyIn) {
+    System.out.println("[과제]");
+    System.out.println("1. 등록");
+    System.out.println("2. 조회");
+    System.out.println("3. 변경");
+    System.out.println("4. 삭제");
+    System.out.println("0. 이전");
+
+    while (true) {
+      String input = prompt("메인/과제", keyIn);
+
+      switch (input) {
+        case "1":
+          System.out.println("등록입니다.");
+          break;
+        case "2":
+          System.out.println("조회입니다.");
+          break;
+        case "3":
+          System.out.println("변경입니다.");
+          break;
+        case "4":
+          System.out.println("삭제입니다.");
+          break;
+        case "0":
+          return;
+        case "menu":
+          System.out.println("[과제]");
+          System.out.println("1. 등록");
+          System.out.println("2. 조회");
+          System.out.println("3. 변경");
+          System.out.println("4. 삭제");
+          System.out.println("0. 이전");
+          break;
+        default:
+          System.out.println("메뉴 번호가 옳지 않습니다!");
+      }
+    }
   }
 
 }
