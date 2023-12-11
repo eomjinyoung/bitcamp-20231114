58d0a95b44b325f7d4be915ef089ce765461ffa2
eomjinyoung <jinyoung.eom@gmail.com>
2023-12-11 16:12:31 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -1,6 +1,7 @@
 package bitcamp.myapp;
 
 import bitcamp.myapp.menu.MenuGroup;
+import bitcamp.myapp.menu.MenuItem;
 import bitcamp.util.Prompt;
 
 public class App {
@@ -12,19 +13,38 @@ public class App {
     MenuGroup mainMenu = new MenuGroup("메인");
 
     MenuGroup assignmentMenu = new MenuGroup("과제");
+    assignmentMenu.add(new MenuItem("등록"));
+    assignmentMenu.add(new MenuItem("조회"));
+    assignmentMenu.add(new MenuItem("변경"));
+    assignmentMenu.add(new MenuItem("삭제"));
+    assignmentMenu.add(new MenuItem("목록"));
     mainMenu.add(assignmentMenu);
 
     MenuGroup boardMenu = new MenuGroup("게시글");
+    boardMenu.add(new MenuItem("등록"));
+    boardMenu.add(new MenuItem("조회"));
+    boardMenu.add(new MenuItem("변경"));
+    boardMenu.add(new MenuItem("삭제"));
+    boardMenu.add(new MenuItem("목록"));
     mainMenu.add(boardMenu);
 
     MenuGroup memberMenu = new MenuGroup("회원");
+    memberMenu.add(new MenuItem("등록"));
+    memberMenu.add(new MenuItem("조회"));
+    memberMenu.add(new MenuItem("변경"));
+    memberMenu.add(new MenuItem("삭제"));
+    memberMenu.add(new MenuItem("목록"));
     mainMenu.add(memberMenu);
 
     MenuGroup greetingMenu = new MenuGroup("가입인사");
+    greetingMenu.add(new MenuItem("등록"));
+    greetingMenu.add(new MenuItem("조회"));
+    greetingMenu.add(new MenuItem("변경"));
+    greetingMenu.add(new MenuItem("삭제"));
+    greetingMenu.add(new MenuItem("목록"));
     mainMenu.add(greetingMenu);
 
-    MenuGroup helpMenu = new MenuGroup("도움말");
-    mainMenu.add(helpMenu);
+    mainMenu.add(new MenuItem("도움말"));
 
     mainMenu.execute(prompt);
 
