0945c02c4603474dc710425a5ffa597ca86820f2
eomjinyoung <jinyoung.eom@gmail.com>
2023-12-11 17:58:03 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/App.java
+++ myapp/app/src/main/java/bitcamp/myapp/App.java
@@ -4,11 +4,8 @@ import bitcamp.menu.MenuGroup;
 import bitcamp.menu.MenuHandler;
 import bitcamp.menu.MenuItem;
 import bitcamp.myapp.menu.BoardAddHandler;
-import bitcamp.myapp.menu.BoardDeleteHandler;
 import bitcamp.myapp.menu.BoardListHandler;
-import bitcamp.myapp.menu.BoardModifyHandler;
 import bitcamp.myapp.menu.BoardRepository;
-import bitcamp.myapp.menu.BoardViewHandler;
 import bitcamp.util.Prompt;
 
 public class App {
@@ -32,7 +29,7 @@ public class App {
     MenuGroup boardMenu = new MenuGroup("게시글");
 
     // 사용자로부터 게시글을 입력 받아서 배열에 저장하는 일을 한다.
-    MenuHandler boardAddHandler = new BoardAddHandler(prompt);
+    MenuHandler boardAddHandler = new BoardAddHandler(boardRepository, prompt);
 
     // "등록" 이라는 메뉴를 선택했을 때 BoardAddHandler를 실행시키는 일을 한다.
     MenuItem boardAddMenu = new MenuItem("등록", boardAddHandler);
@@ -40,10 +37,10 @@ public class App {
     // 게시글 등록을 처리하는 메뉴를 게시글 메뉴에 추가한다.
     boardMenu.add(boardAddMenu);
 
-    boardMenu.add(new MenuItem("조회", new BoardViewHandler()));
-    boardMenu.add(new MenuItem("변경", new BoardModifyHandler()));
-    boardMenu.add(new MenuItem("삭제", new BoardDeleteHandler()));
-    boardMenu.add(new MenuItem("목록", new BoardListHandler()));
+    //boardMenu.add(new MenuItem("조회", new BoardViewHandler()));
+    //boardMenu.add(new MenuItem("변경", new BoardModifyHandler()));
+    //boardMenu.add(new MenuItem("삭제", new BoardDeleteHandler()));
+    boardMenu.add(new MenuItem("목록", new BoardListHandler(boardRepository)));
     mainMenu.add(boardMenu);
 
     MenuGroup memberMenu = new MenuGroup("회원");
