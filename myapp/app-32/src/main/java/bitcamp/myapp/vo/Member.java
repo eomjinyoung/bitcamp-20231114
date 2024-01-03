80fde7c9542b2b6b8bffe16dbf37b30a58f8d879
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-03 17:53:56 +0900

    .

---
+++ myapp/app-32/src/main/java/bitcamp/myapp/vo/Member.java
@@ -0,0 +1,43 @@
+package bitcamp.myapp.vo;
+
+import java.util.Date;
+
+public class Member {
+
+  private String email;
+  private String name;
+  private String password;
+  private Date createdDate;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public Date getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(Date createdDate) {
+    this.createdDate = createdDate;
+  }
+}
