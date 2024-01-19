f2a0af37af03f65ec1ea745cfbe45b7c7c3f5c76
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-19 13:06:51 +0900

    .

---
+++ Hello.java
@@ -0,0 +1,9 @@
+// preview feature
+// - unnamed class + instance main method
+// $ javac --release 21 --enable-preview Hello.java
+// $ java --enable-preview Hello
+//
+void main() {
+  System.out.println("Hello!");
+}
+
