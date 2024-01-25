d2e19acc102672b7f2b74224197b4231b5754e94
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-25 13:38:04 +0900

    .

--- myapp/app-server/src/main/java/bitcamp/util/ThreadPool.java
+++ myapp/app-server/src/main/java/bitcamp/util/ThreadPool.java
@@ -7,6 +7,19 @@ public class ThreadPool implements Pooling<WorkerThread> {
 
   List<WorkerThread> list = new ArrayList<>();
 
+  public ThreadPool() {
+  }
+
+  // 처음에 스레드를 미리 일정 개수 생성해 두기
+  public ThreadPool(int initSize) {
+    if (initSize <= 0 || initSize > 100) {
+      return;
+    }
+    for (int i = 0; i < initSize; i++) {
+      list.add(create());
+    }
+  }
+
   @Override
   public WorkerThread get() {
     if (list.size() > 0) {
@@ -14,18 +27,22 @@ public class ThreadPool implements Pooling<WorkerThread> {
       System.out.printf("기존 스레드를 꺼냄!(%s)\n", t.getName());
       return t;
     }
+    return create();
+  }
+
+  @Override
+  public void revert(WorkerThread thread) {
+    list.add(thread);
+  }
+
+  private WorkerThread create() {
     WorkerThread thread = new WorkerThread(this);
     thread.start();
     try {
       Thread.sleep(500); // 스레드가 wait 할 시간을 확보한다.
     } catch (Exception e) {
     }
-    System.out.printf("새 스레드를 만들어 리턴!(%s)\n", thread.getName());
+    System.out.printf("새 스레드 생성!(%s)\n", thread.getName());
     return thread;
-  }
-
-  @Override
-  public void revert(WorkerThread thread) {
-    list.add(thread);
   }
 }
