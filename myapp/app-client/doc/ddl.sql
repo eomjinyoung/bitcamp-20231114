ce7c26b9406c9026103be57ec973a02b39b04298
eomjinyoung <jinyoung.eom@gmail.com>
2024-01-29 12:30:58 +0900

    .

--- myapp/app-client/doc/ddl.sql
+++ myapp/app-client/doc/ddl.sql
@@ -1,5 +1,7 @@
 -- DDL(Data Definition Language)
 
+drop table boards;
+
 create table boards(
   board_no int primary key auto_increment,
   title varchar(255) not null,
@@ -8,10 +10,38 @@ create table boards(
   created_date datetime null default now()
 );
 
-insert into boards(title,content,writer) values('제목1','내용1','홍길동');
-insert into boards(title,content,writer) values('제목2','내용2','임꺽정');
-insert into boards(title,content,writer) values('제목3','내용3','유관순');
-insert into boards(title,content,writer) values('제목4','내용4','안중근');
-insert into boards(title,content,writer) values('제목5','내용5','윤봉길');
+insert into boards(board_no,title,content,writer) 
+  values(1, '제목1','내용1','홍길동');
+insert into boards(board_no,title,content,writer) 
+  values(2, '제목2','내용2','임꺽정');
+insert into boards(board_no,title,content,writer) 
+  values(3, '제목3','내용3','유관순');
+insert into boards(board_no,title,content,writer) 
+  values(4, '제목4','내용4','안중근');
+insert into boards(board_no,title,content,writer) 
+  values(5, '제목5','내용5','윤봉길');
 
-select * from boards;
+select * 
+from boards;
+
+select * 
+from boards 
+where board_no = 3;
+
+update boards set 
+  title='okok', 
+  content='nono', 
+  writer='hoho' 
+where board_no = 3;
+
+delete from boards where board_no=3;
+
+
+drop table assignment;
+
+create table assignments(
+  assignment_no int primary key auto_increment,
+  title varchar(255) not null,
+  content text not null,
+  deadline date not null
+);
--- myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/BoardDaoImpl.java
+++ myapp/app-client/src/main/java/bitcamp/myapp/dao/mysql/BoardDaoImpl.java
@@ -1,8 +1,12 @@
 package bitcamp.myapp.dao.mysql;
 
 import bitcamp.myapp.dao.BoardDao;
+import bitcamp.myapp.dao.DaoException;
 import bitcamp.myapp.vo.Board;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BoardDaoImpl implements BoardDao {
@@ -17,26 +21,86 @@ public class BoardDaoImpl implements BoardDao {
 
   @Override
   public void add(Board board) {
+    try {
+      Statement stmt = con.createStatement();
+      stmt.executeUpdate(String.format(
+          "insert into boards(title,content,writer) values('%s','%s','%s')",
+          board.getTitle(), board.getContent(), board.getWriter()));
 
+    } catch (Exception e) {
+      throw new DaoException("데이터 입력 오류", e);
+    }
   }
 
   @Override
   public int delete(int no) {
-    return 0;
+    try {
+      Statement stmt = con.createStatement();
+      return stmt.executeUpdate(String.format("delete from boards where board_no=%d", no));
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 입력 오류", e);
+    }
   }
 
   @Override
   public List<Board> findAll() {
-    return null;
+    try {
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("select * from boards");
+
+      ArrayList<Board> list = new ArrayList<>();
+
+      while (rs.next()) {
+        Board board = new Board();
+        board.setNo(rs.getInt("board_no"));
+        board.setTitle(rs.getString("title"));
+        board.setContent(rs.getString("content"));
+        board.setWriter(rs.getString("writer"));
+        board.setCreatedDate(rs.getDate("created_date"));
+
+        list.add(board);
+      }
+      return list;
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 가져오기 오류", e);
+    }
   }
 
   @Override
   public Board findBy(int no) {
-    return null;
+    try {
+      Statement stmt = con.createStatement();
+      ResultSet rs = stmt.executeQuery("select * from boards where board_no = " + no);
+
+      if (rs.next()) {
+        Board board = new Board();
+        board.setNo(rs.getInt("board_no"));
+        board.setTitle(rs.getString("title"));
+        board.setContent(rs.getString("content"));
+        board.setWriter(rs.getString("writer"));
+        board.setCreatedDate(rs.getDate("created_date"));
+
+        return board;
+      }
+      return null;
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 가져오기 오류", e);
+    }
   }
 
   @Override
   public int update(Board board) {
-    return 0;
+    try {
+      Statement stmt = con.createStatement();
+      return stmt.executeUpdate(String.format(
+          "update boards set title='%s', content='%s', writer='%s' where board_no=%d",
+          board.getTitle(), board.getContent(), board.getWriter(), board.getNo()));
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 입력 오류", e);
+    }
   }
 }
