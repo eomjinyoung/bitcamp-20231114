15b94ce2f4350e7838ac9cc9f03ac9c6da00053d
eomjinyoung <jinyoung.eom@gmail.com>
2024-02-16 15:49:32 +0900

    .

---
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/auth/LogoutServlet.java
@@ -0,0 +1,36 @@
+package bitcamp.myapp.servlet.auth;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/auth/logout")
+public class LogoutServlet extends HttpServlet {
+
+  @Override
+  public void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    request.getSession().invalidate();
+
+    response.setContentType("text/html;charset=UTF-8");
+    PrintWriter out = response.getWriter();
+
+    out.println("<!DOCTYPE html>");
+    out.println("<html lang='en'>");
+    out.println("<head>");
+    out.println("  <meta charset='UTF-8'>");
+    out.println("  <title>비트캠프 데브옵스 5기</title>");
+    out.println("</head>");
+    out.println("<body>");
+    out.println("<h1>과제 관리 시스템</h1>");
+    out.println("<h2>로그아웃</h2>");
+    out.println("<p>로그아웃 했습니다!</p>");
+    out.println("</body>");
+    out.println("</html>");
+  }
+}
--- myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardListServlet.java
+++ myapp/app/src/main/java/bitcamp/myapp/servlet/board/BoardListServlet.java
@@ -40,7 +40,7 @@ public class BoardListServlet extends GenericServlet {
     out.println("<body>");
     out.println("<h1>게시글</h1>");
 
-    out.println("<a href=''>새 글</a>");
+    out.println("<a href='/board/form.html'>새 글</a>");
 
     try {
       out.println("<table border='1'>");
