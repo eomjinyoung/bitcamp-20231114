24bc81626c9786847276b2c4eeaed9f80a31694c
eomjinyoung <jinyoung.eom@gmail.com>
2024-02-19 16:05:53 +0900

    .

---
+++ myapp/app-58-web/doc/ddl.sql
@@ -0,0 +1,56 @@
+-- DDL(Data Definition Language)
+
+drop table if exists boards restrict;
+drop table if exists board_files restrict;
+drop table if exists assignments restrict;
+drop table if exists members restrict;
+
+create table boards(
+  board_no int not null,
+  title varchar(255) not null,
+  content text not null,
+  writer int not null,
+  category int not null,
+  created_date datetime null default now()
+);
+
+alter table boards
+  add constraint primary key (board_no),
+  modify column board_no int not null auto_increment;
+
+create table board_files(
+  file_no int not null,
+  file_path varchar(255) not null,
+  board_no int not null
+);
+
+alter table board_files
+  add constraint primary key (file_no),
+  modify column file_no int not null auto_increment,
+  add constraint board_files_fk foreign key (board_no) references boards(board_no);
+
+create table assignments(
+  assignment_no int not null,
+  title varchar(255) not null,
+  content text not null,
+  deadline date not null
+);
+
+alter table assignments
+  add constraint primary key (assignment_no),
+  modify column assignment_no int not null auto_increment;
+
+create table members(
+  member_no int not null,
+  email varchar(255) not null,
+  name varchar(255) not null,
+  password varchar(100) not null,
+  created_date datetime null default now()
+);
+
+alter table members
+  add constraint primary key (member_no),
+  modify column member_no int not null auto_increment;
+
+alter table boards
+  add constraint boards_fk foreign key (writer) references members(member_no);
---
+++ myapp/app-58-web/src/main/java/bitcamp/myapp/dao/mysql/AttachedFileDaoImpl.java
@@ -0,0 +1,132 @@
+package bitcamp.myapp.dao.mysql;
+
+import bitcamp.myapp.dao.AttachedFileDao;
+import bitcamp.myapp.dao.DaoException;
+import bitcamp.myapp.vo.AttachedFile;
+import bitcamp.util.DBConnectionPool;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AttachedFileDaoImpl implements AttachedFileDao {
+
+  DBConnectionPool connectionPool;
+
+  public AttachedFileDaoImpl(DBConnectionPool connectionPool) {
+    this.connectionPool = connectionPool;
+  }
+
+  @Override
+  public void add(AttachedFile file) {
+    try (Connection con = connectionPool.getConnection();
+        PreparedStatement pstmt = con.prepareStatement(
+            "insert into board_files(file_path,board_no) values(?,?)")) {
+
+      pstmt.setString(1, file.getFilePath());
+      pstmt.setInt(2, file.getBoardNo());
+
+      pstmt.executeUpdate();
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 입력 오류", e);
+    }
+  }
+
+  @Override
+  public int addAll(List<AttachedFile> files) {
+    try (Connection con = connectionPool.getConnection();
+        PreparedStatement pstmt = con.prepareStatement(
+            "insert into board_files(file_path,board_no) values(?,?)")) {
+
+      for (AttachedFile file : files) {
+        pstmt.setString(1, file.getFilePath());
+        pstmt.setInt(2, file.getBoardNo());
+        pstmt.executeUpdate();
+      }
+
+      return files.size();
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 입력 오류", e);
+    }
+  }
+
+  @Override
+  public int delete(int no) {
+    try (Connection con = connectionPool.getConnection();
+        PreparedStatement pstmt = con.prepareStatement(
+            "delete from board_files where file_no=?")) {
+      pstmt.setInt(1, no);
+      return pstmt.executeUpdate();
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 삭제 오류", e);
+    }
+  }
+
+  @Override
+  public int deleteAll(int boardNo) {
+    try (Connection con = connectionPool.getConnection();
+        PreparedStatement pstmt = con.prepareStatement(
+            "delete from board_files where board_no=?")) {
+      pstmt.setInt(1, boardNo);
+      return pstmt.executeUpdate();
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 삭제 오류", e);
+    }
+  }
+
+  @Override
+  public List<AttachedFile> findAllByBoardNo(int boardNo) {
+    try (Connection con = connectionPool.getConnection();
+        PreparedStatement pstmt = con.prepareStatement(
+            "select file_no, file_path, board_no"
+                + " from board_files where board_no=? order by file_no asc")) {
+
+      pstmt.setInt(1, boardNo);
+
+      try (ResultSet rs = pstmt.executeQuery()) {
+
+        ArrayList<AttachedFile> list = new ArrayList<>();
+
+        while (rs.next()) {
+          AttachedFile file = new AttachedFile();
+          file.setNo(rs.getInt("file_no"));
+          file.setFilePath(rs.getString("file_path"));
+          file.setBoardNo(rs.getInt("board_no"));
+
+          list.add(file);
+        }
+        return list;
+      }
+
+    } catch (Exception e) {
+      throw new DaoException("데이터 가져오기 오류", e);
+    }
+  }
+
+  @Override
+  public AttachedFile findByNo(int no) {
+    try (Connection con = connectionPool.getConnection();
+        PreparedStatement pstmt = con.prepareStatement(
+            "select file_no, file_path, board_no"
+                + " from board_files where file_no=?")) {
+      pstmt.setInt(1, no);
+      try (ResultSet rs = pstmt.executeQuery()) {
+        if (rs.next()) {
+          AttachedFile file = new AttachedFile();
+          file.setNo(rs.getInt("file_no"));
+          file.setFilePath(rs.getString("file_path"));
+          file.setBoardNo(rs.getInt("board_no"));
+          return file;
+        }
+        return null;
+      }
+    } catch (Exception e) {
+      throw new DaoException("데이터 가져오기 오류", e);
+    }
+  }
+}
