20318e0e9c71020e419d625586b0c90fc5688fb1
eomjinyoung <jinyoung.eom@gmail.com>
2024-03-05 12:14:37 +0900

    .

---
+++ myapp/app-69-web/src/main/java/bitcamp/myapp/controller/HomeController.java
@@ -0,0 +1,13 @@
+package bitcamp.myapp.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HomeController {
+
+  @RequestMapping
+  public String home(HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    return "/home.jsp";
+  }
+}
--- myapp/app/src/main/java/bitcamp/myapp/controller/PageController.java
+++
@@ -1,9 +0,0 @@
-package bitcamp.myapp.controller;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-public interface PageController {
-
-  String execute(HttpServletRequest request, HttpServletResponse response) throws Exception;
-}
