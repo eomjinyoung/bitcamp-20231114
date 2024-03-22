496a8d92343e14440f408264455b72bcbda3ffc3
eomjinyoung <jinyoung.eom@gmail.com>
2024-03-22 11:21:49 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/dao/mysql/MemberDaoImpl.java
+++ myapp/app/src/main/java/bitcamp/myapp/dao/mysql/MemberDaoImpl.java
@@ -1,12 +1,8 @@
 package bitcamp.myapp.dao.mysql;
 
-import bitcamp.myapp.dao.DaoException;
 import bitcamp.myapp.dao.MemberDao;
 import bitcamp.myapp.vo.Member;
-import bitcamp.util.DBConnectionPool;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.util.HashMap;
 import java.util.List;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -18,127 +14,55 @@ import org.springframework.stereotype.Repository;
 public class MemberDaoImpl implements MemberDao {
 
   private final Log log = LogFactory.getLog(this.getClass());
-  DBConnectionPool connectionPool;
   SqlSessionFactory sqlSessionFactory;
 
-  public MemberDaoImpl(DBConnectionPool connectionPool, SqlSessionFactory sqlSessionFactory) {
+  public MemberDaoImpl(SqlSessionFactory sqlSessionFactory) {
     log.debug("MemberDaoImpl() 호출됨!");
-    this.connectionPool = connectionPool;
     this.sqlSessionFactory = sqlSessionFactory;
   }
 
   @Override
   public void add(Member member) {
-    try (Connection con = connectionPool.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(
-            "insert into members(email,name,password,photo) values(?,?,sha2(?,256),?)")) {
-      pstmt.setString(1, member.getEmail());
-      pstmt.setString(2, member.getName());
-      pstmt.setString(3, member.getPassword());
-      pstmt.setString(4, member.getPhoto());
-      pstmt.executeUpdate();
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 입력 오류", e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+      sqlSession.insert("MemberDao.add", member);
     }
   }
 
   @Override
   public int delete(int no) {
-    try (Connection con = connectionPool.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(
-            "delete from members where member_no=?")) {
-      pstmt.setInt(1, no);
-      return pstmt.executeUpdate();
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 삭제 오류", e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+      return sqlSession.delete("MemberDao.delete", no);
     }
   }
 
   @Override
   public List<Member> findAll() {
     try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
-      return sqlSession.selectList("Mapper1.sql1");
+      return sqlSession.selectList("MemberDao.findAll");
     }
   }
 
   @Override
   public Member findBy(int no) {
-    try (Connection con = connectionPool.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(
-            "select member_no, email, name, photo, created_date from members where member_no=?")) {
-      pstmt.setInt(1, no);
-
-      try (ResultSet rs = pstmt.executeQuery()) {
-        if (rs.next()) {
-          Member member = new Member();
-          member.setNo(rs.getInt("member_no"));
-          member.setEmail(rs.getString("email"));
-          member.setName(rs.getString("name"));
-          member.setPhoto(rs.getString("photo"));
-          member.setCreatedDate(rs.getDate("created_date"));
-          return member;
-        }
-        return null;
-      }
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 가져오기 오류", e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      return sqlSession.selectOne("MemberDao.findBy", no);
     }
   }
 
   @Override
   public int update(Member member) {
-    String sql = null;
-    if (member.getPassword().length() == 0) {
-      sql = "update members set email=?, name=?, photo=? where member_no=?";
-    } else {
-      sql = "update members set email=?, name=?, photo=?, password=sha2(?,256) where member_no=?";
-    }
-
-    try (Connection con = connectionPool.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(sql)) {
-      pstmt.setString(1, member.getEmail());
-      pstmt.setString(2, member.getName());
-      pstmt.setString(3, member.getPhoto());
-
-      if (member.getPassword().length() == 0) {
-        pstmt.setInt(4, member.getNo());
-      } else {
-        pstmt.setString(4, member.getPassword());
-        pstmt.setInt(5, member.getNo());
-      }
-
-      return pstmt.executeUpdate();
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 변경 오류", e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession(true)) {
+      return sqlSession.update("MemberDao.update", member);
     }
   }
 
   @Override
   public Member findByEmailAndPassword(String email, String password) {
-    try (Connection con = connectionPool.getConnection();
-        PreparedStatement pstmt = con.prepareStatement(
-            "select member_no, email, name, created_date from members where email=? and password=sha2(?,256)")) {
-      pstmt.setString(1, email);
-      pstmt.setString(2, password);
-
-      try (ResultSet rs = pstmt.executeQuery()) {
-        if (rs.next()) {
-          Member member = new Member();
-          member.setNo(rs.getInt("member_no"));
-          member.setEmail(rs.getString("email"));
-          member.setName(rs.getString("name"));
-          member.setCreatedDate(rs.getDate("created_date"));
-          return member;
-        }
-        return null;
-      }
-
-    } catch (Exception e) {
-      throw new DaoException("데이터 가져오기 오류", e);
+    try (SqlSession sqlSession = sqlSessionFactory.openSession()) {
+      HashMap<String, Object> data = new HashMap<>();
+      data.put("email", email);
+      data.put("password", password);
+      return sqlSession.selectOne("MemberDao.findByEmailAndPassword", data);
     }
   }
 }
