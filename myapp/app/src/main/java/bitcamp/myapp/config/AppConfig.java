c5c6a2d939ee66d574c0466272040e920079447a
eomjinyoung <jinyoung.eom@gmail.com>
2024-03-26 12:53:15 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/config/AppConfig.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/AppConfig.java
@@ -2,12 +2,19 @@ package bitcamp.myapp.config;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.thymeleaf.spring5.ISpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @ComponentScan({"bitcamp.myapp.controller"})
 public class AppConfig {
@@ -23,8 +30,41 @@ public class AppConfig {
     return new StandardServletMultipartResolver();
   }
 
-  @Bean
+  //@Bean
   public ViewResolver viewResolver() {
-    return new InternalResourceViewResolver("/WEB-INF/jsp/", ".jsp");
+    InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+    viewResolver.setPrefix("/WEB-INF/jsp/");
+    viewResolver.setSuffix(".jsp");
+    viewResolver.setOrder(1);
+    return viewResolver;
+  }
+
+  @Bean
+  public ThymeleafViewResolver viewResolver(ISpringTemplateEngine springTemplateEngine) {
+    ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+    viewResolver.setTemplateEngine(springTemplateEngine);
+    //viewResolver.setViewNames(new String[]{"*.html", "*.xhtml"});
+    viewResolver.setCharacterEncoding("UTF-8");
+    viewResolver.setOrder(1);
+    return viewResolver;
+  }
+
+  @Bean
+  public SpringResourceTemplateResolver templateResolver(ApplicationContext applicationContext) {
+    SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+    templateResolver.setApplicationContext(applicationContext);
+    templateResolver.setPrefix("/WEB-INF/templates/");
+    templateResolver.setSuffix(".html");
+    templateResolver.setTemplateMode(TemplateMode.HTML);
+    templateResolver.setCacheable(true);
+    return templateResolver;
+  }
+
+  @Bean
+  public SpringTemplateEngine templateEngine(ITemplateResolver templateResolver) {
+    SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+    templateEngine.setTemplateResolver(templateResolver);
+    templateEngine.setEnableSpringELCompiler(true);
+    return templateEngine;
   }
 }
