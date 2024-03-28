192b892eb7dbf2348fd170f4828f20cc7d5d9d78
eomjinyoung <jinyoung.eom@gmail.com>
2024-03-28 12:53:07 +0900

    .

--- myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
+++ myapp/app/src/main/java/bitcamp/myapp/config/RootConfig.java
@@ -10,12 +10,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@Configuration
 @EnableTransactionManagement
 @MapperScan("bitcamp.myapp.dao")
 @ComponentScan({"bitcamp.myapp.dao", "bitcamp.myapp.service"})
--- myapp/app/src/main/webapp/WEB-INF/jsp/footer.jsp
+++
@@ -1,5 +0,0 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" trimDirectiveWhitespaces="true" %>
-<footer style='background-color:gray; color:white; padding:10px; text-align:center;'>
-  <address> 비트캠프:서울 강남구 강남대로94길 20, 삼오빌딩 5 층 </address>
-  <p>&copy; 2024 네이버클라우드 데브옵스 과정 5기</p>
-</footer>
